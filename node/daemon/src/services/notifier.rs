fd3efafeb90df1419fce6473db5a4a39cb963c4a
Nick <nick@nxcc.org>
2025-03-24 13:09:10 +0100

    daemon: don't send dummy notifications

--- node/daemon/src/services/notifier.rs
+++ node/daemon/src/services/notifier.rs
@@ -10,26 +10,26 @@ pub async fn start_service(mut sender: mpsc::Sender<NotifierMessage>) {
     info!("Starting notifier service");
 
     // Periodically send notifications
-    tokio::spawn(async move {
-        let mut counter = 0;
-        loop {
-            sleep(Duration::from_secs(60)).await;
-            counter += 1;
+    // tokio::spawn(async move {
+    //     let mut counter = 0;
+    //     loop {
+    //         sleep(Duration::from_secs(60)).await;
+    //         counter += 1;
 
-            let notification = format!("Periodic notification #{}", counter);
-            debug!("Sending notification: {}", notification);
+    //         let notification = format!("Periodic notification #{}", counter);
+    //         debug!("Sending notification: {}", notification);
 
-            if let Err(e) = sender.try_send(NotifierMessage::Notification(notification)) {
-                error!("Failed to send notification: {}", e);
-            }
+    //         if let Err(e) = sender.try_send(NotifierMessage::Notification(notification)) {
+    //             error!("Failed to send notification: {}", e);
+    //         }
 
-            // Simulate a response after a short delay
-            sleep(Duration::from_secs(2)).await;
-            let response = format!("Response to notification #{}", counter);
+    //         // Simulate a response after a short delay
+    //         sleep(Duration::from_secs(2)).await;
+    //         let response = format!("Response to notification #{}", counter);
 
-            if let Err(e) = sender.try_send(NotifierMessage::Response(response)) {
-                error!("Failed to send notification response: {}", e);
-            }
-        }
-    });
+    //         if let Err(e) = sender.try_send(NotifierMessage::Response(response)) {
+    //             error!("Failed to send notification response: {}", e);
+    //         }
+    //     }
+    // });
 }
