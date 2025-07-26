8ff851df430b59ec4a193350f07012bf5f85fb70
Nick <nick@nxcc.org>
2025-07-26 16:15:45 +0800

    docs: add new theme

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -32,6 +32,30 @@ export default defineConfig({
         },
       ],
       customCss: ["./src/styles/global.css"],
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap",
+            rel: "stylesheet",
+          },
+        },
+      ],
     }),
     vue(),
   ],
