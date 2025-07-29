03c0fdd372236ab59e065235d79b6d27a4f8ad5a
Nick <nick@nxcc.org>
2025-07-29 20:02:52 +0800

    docs: remove unused astro config item

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -32,14 +32,6 @@ export default defineConfig({
         },
       ],
       customCss: ["./src/styles/global.css"],
-      markdown: {
-        shikiConfig: {
-          themes: {
-            light: "github-light",
-            dark: "github-dark",
-          },
-        },
-      },
     }),
     vue(),
   ],
